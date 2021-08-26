365f2e42df07ece1b68ccb0ce4b83e1f16467fe3
prebur1jrm <prebur1jrm@iHeartMedia.com>
2021-08-26 12:57:00 -0700

    Added allowed hosts

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'gtm+@%okn+k+xz8ynkp$5%t130%d8yr2buqff-91d!4=zb@lit'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'ec2-3-15-203-16.us-east-2.compute.amazonaws.com',
+    '127.0.0.1'
+]
 
 
 # Application definition
